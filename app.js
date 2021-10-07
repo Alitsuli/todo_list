17ae9b7c1aa12dcd66c05b7f929146e42dc388b1
Alitsuli <ali.heidari@metropolia.fi>
2021-10-07 16:59:20 +0300

    lisää nappi tehty ja css korjattu

---
+++ app.js
@@ -0,0 +1,41 @@
+//valitsimet eli selectors
+const todoSyotto = document.querySelector(".todo-syotto");
+const todoNappi = document.querySelector(".todo-nappi");
+const todoLista = document.querySelector(".todo-lista");
+
+
+// Eventti kuuntelija eli event listeners
+todoNappi.addEventListener("click", function(event){
+    //poista turhat eventit eli estää lomakkeen lähettämisen tässä tapauksessa
+    event.preventDefault();
+
+    // luo Todo div
+    const todoDiv = document.createElement("div"); //luo dev elementti
+    todoDiv.classList.add("todo"); //antaa nimen classille
+
+    //luo Li
+    const newTodoLi = document.createElement("li"); //luo li elementti
+    newTodoLi.innerText = todoSyotto.value;
+    newTodoLi.classList.add("todo-item"); //antaa nimen classille
+    todoDiv.appendChild(newTodoLi); //lisää
+
+    // tarkistus nappi
+    const tehtyNappi = document.createElement("button");
+    tehtyNappi.innerHTML = '<i class="fas fa-check"></li>';
+    tehtyNappi.classList.add("tehty-nap");
+    todoDiv.appendChild(tehtyNappi);
+
+    // poista nappi
+    const poistaNappi = document.createElement("button");
+    poistaNappi.innerHTML = '<i class="fas fa-trash"></li>';
+    poistaNappi.classList.add("poista-nap");
+    todoDiv.appendChild(poistaNappi);
+
+    // lisää kaikki listaan
+    todoLista.appendChild(todoDiv);
+
+    //tyhjentää teksti kenttä
+    todoSyotto.value = "";
+});
+
+// funktiot eli function
